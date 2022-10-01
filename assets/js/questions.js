b4ea878dedcb96fd4572eb34e8c53d5c9efb7ed7
squrpe <83641173+squrpe@users.noreply.github.com>
2022-10-01 22:38:07 +0930

    Changed overall code

---
+++ assets/js/questions.js
@@ -0,0 +1,42 @@
+var questions = [
+    {
+        title: 'What is a?',
+        a: 'a',
+        b: 'b',
+        c: 'c',
+        d: 'd',
+        answer: 'a'
+    },
+    {
+        title: 'What is b?',
+        a: 'a',
+        b: 'b',
+        c: 'c',
+        d: 'd',
+        answer: 'b'
+    },
+    {
+        title: 'What is c?',
+        a: 'a',
+        b: 'b',
+        c: 'c',
+        d: 'd',
+        answer: 'c'
+    },
+    {
+        title: 'What is d?',
+        a: 'a',
+        b: 'b',
+        c: 'c',
+        d: 'd',
+        answer: 'd'
+    },
+    {
+      title: 'What is a?',
+      a: 'a',
+      b: 'b',
+      c: 'c',
+      d: 'd',
+      answer: 'a'
+  },
+];
